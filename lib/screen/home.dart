3feb4eefcacac04dfada1ecad89c983c7ed46584
RosisHudaya <rosishudayaputra02@gmail.com>
2022-11-26 20:51:27 +0700

    listview

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_api/screen/first_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_api/components/category_models.dart';
+
+import '../api/http_helper.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -7,8 +14,53 @@ class Home extends StatefulWidget {
 }
 
 class _Home extends State<Home> {
-  String name = 'Huda';
-  String email = 'huda02@gmail.com';
+  String token = '';
+  String name = '';
+  String email = '';
+  List listCategory = [];
+
+  getPref() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    setState(() {
+      const key = 'token';
+      const key1 = 'name';
+      const key2 = 'email';
+      final value = pref.get(key);
+      final value1 = pref.get(key1);
+      final value2 = pref.get(key2);
+      token = '$value';
+      name = '$value1';
+      email = '$value2';
+    });
+  }
+
+  getKategori() async {
+    final response = await HttpHelper().getKategori();
+    var dataResponse = jsonDecode(response.body);
+    setState(() {
+      var listRespon = dataResponse['data'];
+      for (var i = 0; i < listRespon.length; i++) {
+        listCategory.add(Category.fromJson(listRespon[i]));
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    getPref();
+    super.initState();
+    getKategori();
+  }
+
+  logOut() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    setState(() {
+      preferences.remove("token");
+      preferences.clear();
+    });
+    final response = await HttpHelper().logout(token);
+    print(response.body);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +79,7 @@ class _Home extends State<Home> {
               children: [
                 Padding(
                   padding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
                   child: Align(
                     alignment: Alignment.centerLeft,
                     child: Text(
@@ -35,7 +87,7 @@ class _Home extends State<Home> {
                       style: TextStyle(
                         color: Colors.white,
                         fontWeight: FontWeight.w900,
-                        fontSize: 50,
+                        fontSize: 25,
                         shadows: [
                           Shadow(
                             color: Colors.purple.shade300,
@@ -48,7 +100,7 @@ class _Home extends State<Home> {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 35),
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
                   child: Align(
                     alignment: Alignment.centerLeft,
                     child: Text(
@@ -58,6 +110,30 @@ class _Home extends State<Home> {
                         fontWeight: FontWeight.bold,
                         fontSize: 15,
                       ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: IconButton(
+                      icon: const Icon(
+                        Icons.logout_sharp,
+                        color: Colors.white,
+                        size: 29,
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return const FirstPage();
+                            },
+                          ),
+                        );
+                        logOut();
+                      },
                     ),
                   ),
                 ),
@@ -72,21 +148,84 @@ class _Home extends State<Home> {
               decoration: BoxDecoration(
                 color: Colors.purple.shade50,
                 borderRadius: const BorderRadius.only(
-                    topRight: Radius.circular(10.0),
-                    topLeft: Radius.circular(10.0)),
+                  topRight: Radius.circular(10.0),
+                  topLeft: Radius.circular(10.0),
+                  // bottomRight: Radius.circular(10.0),
+                  // bottomLeft: Radius.circular(10.0),
+                ),
               ),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Center(
-                      child: Text(
-                        'Hallo Dek',
-                        textAlign: TextAlign.left,
+              child: ListView.builder(
+                itemCount: listCategory.length,
+                itemBuilder: (context, index) {
+                  var kategori = listCategory[index];
+                  return Dismissible(
+                    key: UniqueKey(),
+                    background: Container(
+                      color: Colors.greenAccent,
+                      child: Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Row(
+                          children: const [
+                            Icon(
+                              Icons.create_rounded,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ]),
+                    secondaryBackground: Container(
+                      color: Colors.redAccent,
+                      child: Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: const [
+                            Icon(
+                              Icons.delete,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    child: Container(
+                      margin: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          top: BorderSide(
+                            color: Colors.deepPurpleAccent,
+                            width: 1.5,
+                          ),
+                          left: BorderSide(
+                            color: Colors.deepPurpleAccent,
+                            width: 1.5,
+                          ),
+                          right: BorderSide(
+                            color: Colors.deepPurpleAccent,
+                            width: 1.5,
+                          ),
+                          bottom: BorderSide(
+                            color: Colors.deepPurpleAccent,
+                            width: 1.5,
+                          ),
+                        ),
+                      ),
+                      child: ListTile(
+                        title: Text(
+                          kategori.name,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.normal,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
